2a7e6818b81e453c4619a147a4bdb4dedf7f1158
jerismith32 <jerismith32@gmail.com>
2021-09-10 15:47:58 -0400

    updated the comment-seeds.js file

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,7 +2,19 @@ const { Comment } = require('../models');
 
 const commentSeedData = [
     {
-
+        comment_body: 'I will definitely be attending the celebrations!',
+        user_id: 2,
+        post_id: 2
+    },
+    {
+        comment_body: 'I am only going because I have to!',
+        user_id: 3,
+        post_id: 2
+    },
+    {
+        comment_body: 'Aww shucks Minnie, you are too kind!',
+        user_id: 1,
+        post_id: 3
     }
 ];
 
