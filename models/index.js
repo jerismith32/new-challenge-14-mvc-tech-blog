e89d83d8d2504e9b805161942acab03a0cf0ab6a
jerismith32 <jerismith32@gmail.com>
2021-09-10 14:23:25 -0400

    updated index.js in models so that it would point to the user, blogpost, and comment model respectively

--- models/index.js
+++ models/index.js
@@ -1,3 +1,7 @@
 //This is going to conenct to all the other models for the App 
-
+const User = require('./User');
+const BlogPost = require('./BlogPost');
+const Comment = require('./Comment');
 //This will create all the associations or interactions for the app
+
+module.exports = { User, BlogPost, Comment }
