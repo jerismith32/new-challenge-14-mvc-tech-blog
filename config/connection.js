987c144bcb57da1c4a56a51865b059a4a6441191
jerismith32 <jerismith32@gmail.com>
2021-09-10 14:09:12 -0400

    updated the connection.js with the connection to sequelize, utilizing dotenv, AND updated the .env file to have the DB_NAME, DB_USER, and DB_PW

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+//Calls in Sequelize
+const Sequelize = require('sequelize');
+//Utilizies dotenv and the .env file (where the database name, username, and password will go)
+require('dotenv').config();
+
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+
+module.exports = sequelize;
