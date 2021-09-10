6d4236410912d6558d7f88849a495dd049617058
jerismith32 <jerismith32@gmail.com>
2021-09-10 15:03:21 -0400

    updated index.js in model directory to create all the associations between user, blog post, and comment models

--- models/index.js
+++ models/index.js
@@ -2,6 +2,35 @@
 const User = require('./User');
 const BlogPost = require('./BlogPost');
 const Comment = require('./Comment');
+
 //This will create all the associations or interactions for the app
+
+User.hasMany(BlogPost, {
+    foreignKey: 'user_id'
+});
+  
+BlogPost.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+  
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+  
+Comment.belongsTo(BlogPost, {
+    foreignKey: 'blogpost_id',
+    onDelete: 'SET NULL'
+});
+  
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+  
+BlogPost.hasMany(Comment, {
+    foreignKey: 'blogpost_id'
+});
 
 module.exports = { User, BlogPost, Comment }
