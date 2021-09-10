6e90020af3a2d8035b5a5509234c5254a394437a
jerismith32 <jerismith32@gmail.com>
2021-09-10 14:43:56 -0400

    update User model to require username, email, and password. Will assign ID within the database

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # new-challenge-14-mvc-tech-blog
+
+[Validation and Contraints](https://sequelize.org/master/manual/validations-and-constraints.html)
--- models/User.js
+++ models/User.js
@@ -3,3 +3,69 @@ const { Model, DataTypes } = require('sequelize');
 //so we can protect our passwords
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
+
+class User extends Model {
+//This will check the password that user is using to login to the password we have stored in our database
+checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+//This is how we will create our user
+//This will make sure every user makes their own username, email, and password
+//They will be assigned a unique id within our database
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+          notNull: {
+              msg: 'Please enter your desired username!'
+          }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6]
+      }
+    }
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+//Exports the module so we can use the it 
+module.exports = User;
