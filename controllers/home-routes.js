929761eb6fe408f0ce73ce43e7fc1d737e4485b4
jerismith32 <jerismith32@gmail.com>
2021-09-10 15:24:37 -0400

    updated home_routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,36 @@
 const router = require('express').Router();
+const { post } = require('.');
 //connects to the connection.js file in the config directory
 const sequelize = require('../config/connection');
 //Pulls in our models
 const { User, BlogPost, Comment } = require('../models');
+
+//We want to get all the blog posts for the homepage
+router.get('/', (req, res) => {
+    //Finds all Blog posts and includes blog title and blog body
+    BlogPost.findAll({
+        attributes: [
+            'blog_title',
+            'blog_body'
+        ],
+        //Will also include the associated user (creator of the blog post)
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            //Includes any comments on the blog post
+            {
+                model: Comment,
+                attributes: 
+                [
+                    'comment_body'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+})
