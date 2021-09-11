fb37f9b4965e6c581f8be76fe4becc896274898a
jerismith32 <jerismith32@gmail.com>
2021-09-11 15:01:13 -0400

    updated user-routes.js with the route for a new user to create an account including their username, email, and password

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,4 +2,27 @@ const router = require('express').Router();
 //This will allow us to use our models for User, Blogpost and Comment
 const { User, Blogpost, Comment } = require('../../models');
 
+//Route for when a new user creates an account
+//Ensures saving of username, email, and password
+router.post('/', (req, res) => {
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+    
+          res.json(dbUserData);
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 module.exports = router;
