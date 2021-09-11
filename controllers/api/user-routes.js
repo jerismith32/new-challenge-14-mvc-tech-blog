ccc7840677e53c360371b5ea030c2f4639c6be89
jerismith32 <jerismith32@gmail.com>
2021-09-11 15:43:04 -0400

    updated route for when an exisiting user logins in

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -23,6 +23,35 @@ router.post('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
-  });
+});
+
+// This will be the route for when an existing user logs in
+router.post('/login', (req, res) => {
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'Incorrect credentials!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect credentials!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+    });
+});
 
 module.exports = router;
