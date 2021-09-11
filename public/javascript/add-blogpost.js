aa613349d893d5ab8223b72bbe17e116831b81b9
jerismith32 <jerismith32@gmail.com>
2021-09-11 17:21:23 -0400

    updated so add-blogpost.js has the functionality for a user to create a blogpost

---
+++ public/javascript/add-blogpost.js
@@ -0,0 +1,26 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const blog_title = document.querySelector('input[name="blog_title"]').value;
+    const blog_body = document.querySelector('input[name="blog_body"]').value;
+  
+    const response = await fetch(`/api/blogposts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        blog_title,
+        blog_body
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+  
