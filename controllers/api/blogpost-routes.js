28b6529b2c88fb7ad6b7edc45f44af804691c382
jerismith32 <jerismith32@gmail.com>
2021-09-12 08:43:40 -0400

    updated initial route on blogpost-routes.js in order to find all Blogposts with the associated users and comments

--- controllers/api/blogpost-routes.js
+++ controllers/api/blogpost-routes.js
@@ -6,4 +6,35 @@ const { User, Blogpost, Comment } = require('../../models');
 //Utilizes our authentication
 const withAuth = require('../../utils/auth');
 
+// Route to find all Posts with their associate Users and Comments
+router.get('/', (req, res) => {
+    //console.log('======================');
+    Blogpost.findAll({
+      attributes: [
+        'id',
+        'blog_title',
+        'blog_body'
+      ],
+      include: [
+        {
+            model: User,
+            attributes: ['username']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_body', 'blogpost_id', 'user_id'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+      ]
+    })
+    .then(dbBlogpostData => res.json(dbBlogpostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
