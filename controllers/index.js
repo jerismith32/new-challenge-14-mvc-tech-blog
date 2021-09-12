e93d0f11de38017cc6f8edcf3de67bcc4fb97d8c
jerismith32 <jerismith32@gmail.com>
2021-09-12 08:36:57 -0400

    updated index.js with comments to explain what each file in the controller directory does

--- controllers/index.js
+++ controllers/index.js
@@ -4,8 +4,11 @@ const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 const apiRoutes = require('./api/');
 
+//This allows us to point to the home-routes.js file
 router.use('/', homeRoutes);
+//This allows us to point to the dashboard-routes.js file
 router.use('/dashboard', dashboardRoutes);
+//This allows us to point to the api directory which holds the routes for each model
 router.use('/api', apiRoutes);
 
 module.exports = router;
