411697fe103b232bfbf643103004e009fe66a9b4
jerismith32 <jerismith32@gmail.com>
2021-09-12 09:18:27 -0400

    updated comment-routes.js to have the route to find all comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -4,4 +4,15 @@ const { Comment } = require('../../models');
 //Utilizes the Authentication
 const withAuth = require('../../utils/auth');
 
+//Route to Find All Comments
+router.get('/', (req, res) => {
+    Comment.findAll()
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+
 module.exports = router;
