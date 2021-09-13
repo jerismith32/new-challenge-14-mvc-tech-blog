b0a03f31fcb36c15c3735cc88bb355ee79a16e6a
jerismith32 <jerismith32@gmail.com>
2021-09-13 10:41:24 -0400

    updated so that your blog posts populate

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -4,17 +4,17 @@ const commentdata = [
     {
         comment_body: 'I will definitely be attending the celebrations!',
         user_id: 2,
-        post_id: 2
+        blogpost_id: 2
     },
     {
         comment_body: 'I am only going because I have to!',
         user_id: 3,
-        post_id: 2
+        blogpost_id: 2
     },
     {
         comment_body: 'Aww shucks Minnie, you are too kind!',
         user_id: 1,
-        post_id: 3
+        blogpost_id: 3
     }
 ];
 
