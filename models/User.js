1a81c0237e9168d5a840a389ef6ed320a2ebaae7
jerismith32 <jerismith32@gmail.com>
2021-09-13 10:14:29 -0400

    updated an uninstalled bycrypt and installed bcryptjs

--- models/User.js
+++ models/User.js
@@ -1,13 +1,13 @@
 // This allows us to use models and datatypes
 const { Model, DataTypes } = require('sequelize');
 //so we can protect our passwords
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 const sequelize = require('../config/connection');
 
 class User extends Model {
 //This will check the password that user is using to login to the password we have stored in our database
 checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
+    return bcryptjs.compareSync(loginPw, this.password);
   }
 }
 
@@ -54,12 +54,12 @@ User.init(
   {
     hooks: {
       async beforeCreate(newUserData) {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        newUserData.password = await bcryptjs.hash(newUserData.password, 10);
         return newUserData;
       },
 
       async beforeUpdate(updatedUserData) {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        updatedUserData.password = await bcryptjs.hash(updatedUserData.password, 10);
         return updatedUserData;
       }
     },
